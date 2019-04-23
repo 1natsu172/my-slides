56835cd29fc13054536696651fcb68776fa3db85
1natsu172 <1natsu172@users.noreply.github.com>
2019-04-23 22:25:42 +0900

    build(scripts): static assets hosting & redirects

--- scripts/generate-now-json.ts
+++ scripts/generate-now-json.ts
@@ -2,6 +2,7 @@ import { readdirSync, writeFileSync } from 'fs'
 import * as path from 'path'
 
 const SLIDES_DIR = process.env.npm_package_config_slidesDir
+const SHARED_ASSETS_DIR = process.env.npm_package_config_sharedAssetsDir
 
 const slideRepoNames: string[] = readdirSync(SLIDES_DIR).map(
   slideName => slideName
@@ -14,7 +15,11 @@ const fixedObject = {
   alias: ['slides-1natsu.now.sh']
 }
 
-const build = slideRepoNames.map(slideName => ({
+const staticAssetsBuild = [
+  { src: `${SHARED_ASSETS_DIR}/images/*`, use: '@now/static' }
+]
+
+const slidesBuild = slideRepoNames.map(slideName => ({
   src: `${SLIDES_DIR}/${slideName}/package.json`,
   use: '@now/static-build',
   config: {
@@ -22,22 +27,32 @@ const build = slideRepoNames.map(slideName => ({
   }
 }))
 
-const routes = slideRepoNames.reduce(
-  (acc, slideName) => [
-    ...acc,
-    {
-      src: `/${slideName}/main.js`,
-      dest: `/packages/${slideName}/main.js`
-    },
-    {
-      src: `/${slideName}/(.*)`,
-      dest: `/packages/${slideName}/index.html`
-    }
-  ],
-  []
-)
+// const routes = slideRepoNames.reduce(
+//   (acc, slideName) => [
+//     ...acc,
+//     {
+//       src: `/${slideName}/main.js`,
+//       dest: `/slides/${slideName}/main.js`
+//     },
+//     {
+//       src: `/${slideName}/(.*)`,
+//       dest: `/slides/${slideName}/$1index.html`
+//     }
+//   ],
+//   []
+// )
+const routes = [
+  {
+    src: '/(.*)', // when user accessed /slideName
+    dest: '/slides/$1' // refer /slides/slideName
+  }
+]
 
-const json = JSON.stringify({ ...fixedObject, build, routes }, null, 2)
+const json = JSON.stringify(
+  { ...fixedObject, builds: [...staticAssetsBuild, ...slidesBuild], routes },
+  null,
+  2
+)
 
 try {
   writeFileSync(path.resolve(process.cwd(), 'now.json'), json)
